8f6ea74382a137c6f47a019735ba71e24198fb14
Tyler Mathews <mrmathews08@gmail.com>
2020-10-01 21:37:00 -0600

    small edits

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     // This file just does a GET request to figure out which user is logged in
     // and updates the HTML on the page
     $.get("/api/allList", function (req, res) {
+      console.log("entered members.js");
       res.render("members", res);
     });
 
