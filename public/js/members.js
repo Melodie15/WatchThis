d4859c8df90bafd789923864566bf8911d19734c
TLCroatt <trishacroatt@gmail.com>
2020-10-03 13:53:39 -0600

    adding post request

--- public/js/members.js
+++ public/js/members.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-arrow-callback */
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
@@ -6,5 +7,21 @@ $(document).ready(() => {
     console.log(res);
     console.log(res.data);
     res.render("members", { movieList: res.data });
+  });
+
+  $("#addItemForm").on("submit", function(event) {
+    event.preventDefault();
+
+    const newMovie = {
+      title: $("#title")
+        .val()
+        .trim(),
+      genre: $("#genre").val(),
+      service: $("#service").val()
+    };
+
+    $.post("/api/new", newMovie).then(function(data) {
+      console.log(data);
+    });
   });
 });
